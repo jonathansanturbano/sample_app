28c76af8dba4451ae485a2dd8ca578b2edcc1fe8
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-06-15 12:11:36 +0200

    Finish user actions

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,7 +9,19 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', about_path
     assert_select 'a[href=?]', help_path
     assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', login_path
     get contact_path
     assert_select "title", full_title("Contact")
+    user = users(:michael)
+    log_in_as(user)
+    get root_path
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', user_path(user)
+    assert_select 'a[href=?]', edit_user_path(user)
+    assert_select 'a[href=?]', logout_path
+    assert_select 'a[href=?]', login_path, count: 0
   end
 end
