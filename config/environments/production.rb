c9fb9f9ba24c1ad4950f73006997432d0819a583
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-06-15 17:34:21 +0200

    Production settings change

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://floating-peak-47573.herokuapp.com/'
+  host = 'floating-peak-47573.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
